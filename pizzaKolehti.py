24ac9ad3db6358c10c26129bb844643cdc5f5d61
XyliX <kerk.pelt@gmail.com>
2015-08-22 16:56:38 +0300

    Pizzakolehti was integrated into "komennonluku"

--- pizzaKolehti.py
+++
@@ -1,8 +0,0 @@
-pizzaList = []
-velkaList = []
-#order = (tilaaja, pizza, hinta)
-def addOrder (user, pizza, order):
-    paid = False
-    pizzaList.append(    (user, pizza, order, paid)    )
-addOrder ("xylix", "al-capone", 12)
-addOrder ("Ege", "tropicana", 12)
