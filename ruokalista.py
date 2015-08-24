69702c3408c5c98fc5ff3a5ed932771e15743e93
XyliX <kerk.pelt@gmail.com>
2015-08-24 10:30:40 +0300

    Made sql injections of over 6 characters impossible.

--- ruokalista.py
+++ ruokalista.py
@@ -1,10 +1,19 @@
 import sqlite3
-import json
+#import json
 sqlite_file = 'pizzat.sqlite'
 conn = sqlite3.connect(sqlite_file)
 c = conn.cursor()
 
 def printTable(table):
-    query = "SELECT * FROM "+table+";"
-    print (repr(query))
-    return [q for q in c.execute(query)]
+    table = table[0:6]
+    return c.execute("SELECT * FROM "+table+";")
+#    separator = ", "
+    ret = ""
+    for row in c.execute("SELECT * FROM "+table+";"):
+        ret += separator.join(str(i) for i in row) + "\n"
+    return ret
+
+
+    # query = "SELECT * FROM "+table+";"
+    # print (repr(query))
+    # return [q for q in c.execute(query)]
